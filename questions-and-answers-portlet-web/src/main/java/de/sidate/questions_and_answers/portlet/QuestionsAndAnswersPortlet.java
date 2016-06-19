cdc901024172b4356ab02ae56273b77df4c90b5b
AdHominem <vargsmane@gmx.de>
2016-06-19 17:37:02 +0200

    Asking Questions works

--- questions-and-answers-portlet-web/src/main/java/de/sidate/questions_and_answers/portlet/QuestionsAndAnswersPortlet.java
+++ questions-and-answers-portlet-web/src/main/java/de/sidate/questions_and_answers/portlet/QuestionsAndAnswersPortlet.java
@@ -13,11 +13,13 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import de.sidate.questions_and_answers.model.Question;
 import de.sidate.questions_and_answers.service.QuestionLocalServiceUtil;
+import de.sidate.questions_and_answers.service.persistence.QuestionPersistence;
+import de.sidate.questions_and_answers.service.persistence.impl.QuestionPersistenceImpl;
 import org.osgi.service.component.annotations.Component;
 
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
-import javax.portlet.Portlet;
+import javax.portlet.*;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.joining;
@@ -28,7 +30,7 @@ import static java.util.stream.Collectors.joining;
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=questions-and-answers-portlet Portlet",
+		"javax.portlet.display-name=Questions and Answers",
 		"javax.portlet.security-role-ref=power-user,user",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
@@ -70,4 +72,25 @@ public class QuestionsAndAnswersPortlet extends MVCPortlet {
 		}
 
 	}
+
+    @Override
+    public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException {
+
+        try {
+            ServiceContext serviceContext = ServiceContextFactory.getInstance(Question.class.getName(), renderRequest);
+            long groupID = serviceContext.getScopeGroupId();
+
+            ArrayList<Question> questions = new ArrayList<>(QuestionLocalServiceUtil.getQuestions(groupID));
+
+            System.out.println(!questions.isEmpty());
+            if (!questions.isEmpty()) {
+                renderRequest.setAttribute("questions", questions);
+            }
+
+            super.render(renderRequest, renderResponse);
+        } catch (PortalException | PortletException e) {
+            log.error(e.getClass().getName() + "\n" + e.getMessage());
+        }
+
+    }
 }
--- questions-and-answers-portlet-web/src/main/resources/META-INF/resources/view.jsp
+++ questions-and-answers-portlet-web/src/main/resources/META-INF/resources/view.jsp
@@ -2,36 +2,26 @@
 <link rel="stylesheet" href="<%=request.getContextPath()%>/css/style.css"/>
 <h1><liferay-ui:message key="questions_and_answersde.sidate.questions_and_answers.portletQuestionsAndAnswersPortlet.caption"/></h1>
 
-<ul class="questions question list-group">
-    <li class="list-group-item">
-        <i class="isAnswered glyphicon glyphicon-ok"></i>
-        <div class="questionText">Das ist eine tolle Frage!</div>
-        Kategorien:
-        <ul class="categories">
-            <li>ISMS</li>
-            <li>Blub</li>
-        </ul>
-        Tags:
-        <ul class="tags">
-            <li>ISMS</li>
-            <li>Blub</li>
-        </ul>
-
-    </li>
-</ul>
-
-<portlet:actionURL name="addCategory" var="addCategoryURL"/>
 <portlet:renderURL var="newQuestionURL">
     <portlet:param name="mvcPath" value="/newQuestion.jsp"/>
 </portlet:renderURL>
 
 <p>;-)</p>
 
-<aui:form action="<%= addCategoryURL%>" name="<portlet:namespace />fm">
-    <aui:fieldset>
-        <aui:input name="name"/>
-        <aui:input name="color"/>
-    </aui:fieldset>
-    <aui:button type="submit"/>
-    <aui:button onClick="<%= newQuestionURL%>" value="New Question"/>
-</aui:form>
+<jsp:useBean id="questions" class="java.util.ArrayList" scope="request"/>
+
+<liferay-ui:search-container>
+    <liferay-ui:search-container-results results="<%= questions %>"/>
+
+    <liferay-ui:search-container-row className="de.sidate.questions_and_answers.model.Question" modelVar="question">
+        <liferay-ui:search-container-column-text property="title" />
+
+        <liferay-ui:search-container-column-text property="text" />
+    </liferay-ui:search-container-row>
+
+    <liferay-ui:search-iterator />
+</liferay-ui:search-container>
+
+<aui:button-row>
+    <aui:button onClick="<%= newQuestionURL%>" value="New Question"></aui:button>
+</aui:button-row>
