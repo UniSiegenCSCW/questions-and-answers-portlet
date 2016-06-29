e8d39d5e059fd8df7d99d9d440715ab447c69b12
bentley <l.ben@gmx.de>
2016-06-29 18:07:19 +0200

    Erlaubte Stringgroesse in portlet-model-hints.xml fuer Textfelder angepasst

--- questions-and-answers-portlet-service/src/main/java/org/sidate/questions_and_answers/model/impl/QuestionImpl.java
+++ questions-and-answers-portlet-service/src/main/java/org/sidate/questions_and_answers/model/impl/QuestionImpl.java
@@ -23,7 +23,7 @@ import java.util.Date;
  * The extended model implementation for the Question service. Represents a row in the &quot;SIDATE_Question&quot; database table, with each column mapped to a property of this class.
  *
  * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link de.sidate.questions_and_answers.model.Question} interface.
+ * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link org.sidate.questions_and_answers.model.Question} interface.
  * </p>
  *
  * @author Brian Wing Shun Chan
@@ -33,7 +33,7 @@ public class QuestionImpl extends QuestionBaseImpl {
 	/*
 	 * NOTE FOR DEVELOPERS:
 	 *
-	 * Never reference this class directly. All methods that expect a question model instance should use the {@link de.sidate.questions_and_answers.model.Question} interface instead.
+	 * Never reference this class directly. All methods that expect a question model instance should use the {@link org.sidate.questions_and_answers.model.Question} interface instead.
 	 */
 	public QuestionImpl() {
 	}
