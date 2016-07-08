f0d5d3bb5b7565add9b89687cbe387f35f8c25be
AdHominem <vargsmane@gmx.de>
2016-07-08 13:54:07 +0200

    + Moved and renamed acceptAnswer

--- questions-and-answers-portlet-web/src/main/java/org/sidate/questions_and_answers/portlet/QuestionsAndAnswersPortlet.java
+++ questions-and-answers-portlet-web/src/main/java/org/sidate/questions_and_answers/portlet/QuestionsAndAnswersPortlet.java
@@ -1,7 +1,6 @@
 package org.sidate.questions_and_answers.portlet;
 
 import com.liferay.asset.kernel.model.AssetEntry;
-import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -10,7 +9,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.service.UserServiceUtil;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -70,6 +68,18 @@ public class QuestionsAndAnswersPortlet extends MVCPortlet {
             response.setRenderParameter("mvcPath", "/view.jsp");
             log.error(e.getClass().getName() + "\n" + e.getMessage());
         }
+
+    }
+
+    /**
+     * Sets the correct answer ID, may also be used to unset the correct answer ID by
+     * passing 0 as the ID parameter.
+     * @throws PortalException
+     */
+    public void setCorrectAnswer(ActionRequest request, ActionResponse response, long questionId, long answerId) throws PortalException {
+        ServiceContext serviceContext = ServiceContextFactory.getInstance(Answer.class.getName(), request);
+        QuestionLocalServiceUtil.setCorrectAnswer(answerId, questionId, serviceContext);
+        SessionMessages.add(request, "answerAccepted");
 
     }
 
@@ -137,12 +147,6 @@ public class QuestionsAndAnswersPortlet extends MVCPortlet {
         }
     }
 
-    public void acceptAnswer(ActionRequest request, ActionResponse response, long questionId, long answerId) throws PortalException {
-        ServiceContext serviceContext = ServiceContextFactory.getInstance(Answer.class.getName(), request);
-        QuestionLocalServiceUtil.setCorrectAnswer(answerId, questionId, serviceContext);
-        SessionMessages.add(request, "answerAccepted");
-
-    }
 
     public void deleteAnswer(ActionRequest request, ActionResponse response) throws PortalException{
         ServiceContext serviceContext = ServiceContextFactory.getInstance(Answer.class.getName(), request);
@@ -258,7 +262,7 @@ public class QuestionsAndAnswersPortlet extends MVCPortlet {
             List<Answer> answers = AnswerLocalServiceUtil.getAnswersForQuestion(question.getQuestionID());
             Answer answer = answers.get(0);
 
-            acceptAnswer(request,response, question.getQuestionID(), answer.getAnswerID());
+            setCorrectAnswer(request,response, question.getQuestionID(), answer.getAnswerID());
         } catch (PortalException e) {
             e.printStackTrace();
         }
